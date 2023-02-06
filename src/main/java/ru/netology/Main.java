eed16f324f1eb74a5a5e3d7eb512cc7697455eaf
Olia Brizgina <olvitbriz@gmail.com>
2023-02-06 21:10:02 +0400

    init commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.surname = "Иванов";
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.passport = "4444№ 444444";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class Post {
+    public String surname;
+    public String name;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday;
+
+}
